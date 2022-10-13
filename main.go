99eff7ec99a0dea5bc3f399355826d53858d219d
DattaAnupam <aanupamdatta.1995@gmail.com>
2022-10-13 10:40:08 +0530

    restructe the program

--- main.go
+++ main.go
@@ -2,43 +2,21 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
+	"go-with-sql/pkg/services"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-type dbtable struct {
-	id        int
-	firstname string
-	lastname  string
-}
-
 func main() {
-	db, err := sql.Open("mysql", "root:MyPass123!mYsql@tcp(127.0.0.1:3306)/mytestdb")
+	// Note
+	// To build and run please replace <Password> with your MySql Root password
+	dsn := "root:<Password>@tcp(127.0.0.1:3306)/mytestdb"
+	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		panic(err.Error())
 	}
 	defer db.Close()
 
-	results, err := db.Query("SELECT * FROM table1")
-	if err != nil {
-		panic(err.Error())
-	}
-
-	for results.Next() {
-		var dbtable1 dbtable
-		err = results.Scan(&dbtable1.id, &dbtable1.firstname, &dbtable1.lastname)
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("Id: %d, Firstname: %s, Lastname: %s\n", dbtable1.id, dbtable1.firstname, dbtable1.lastname)
-	}
-
-	fmt.Println("Success!!!Value Received")
-	// insert, err := db.Query("INSERT INTO table1 VALUES(1, 'Anupam', 'Datta')")
-	// if err != nil {
-	// 	panic(err.Error())
-	// }
-	// defer insert.Close()
-	// fmt.Println("Success!!!Value Added")
+	services.AddValuesToDb(db)
+	services.RetrieveValuesFromDb(db)
 }
