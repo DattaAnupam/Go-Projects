7af964d99c195f80bc7e4cb5d376d99bc534db0d
DattaAnupam <aanupamdatta.1995@gmail.com>
2022-10-14 08:24:56 +0530

    declare and define book model

--- pkg/models/book-management-models.go
+++ pkg/models/book-management-models.go
@@ -2,7 +2,16 @@ package models
 
 import (
 	"fmt"
+
+	"gorm.io/gorm"
 )
+
+type Book struct {
+	gorm.Model
+	BookName string `json:"bookname"`
+	Author   string `json:"author"`
+	Isbn     string `json:"isbn"`
+}
 
 func TryModels() {
 	fmt.Println("from pkg/models/book-management-models.go")
