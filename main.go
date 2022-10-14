eeb092ffe690c0a07b20dd79f596835af7e60311
DattaAnupam <aanupamdatta.1995@gmail.com>
2022-10-14 07:27:31 +0530

    add models directory

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"main/pkg/controllers"
+	"main/pkg/models"
 	"main/pkg/routes"
 )
 
@@ -10,4 +11,5 @@ func main() {
 	fmt.Println("basic structure")
 	routes.TryFunc()
 	controllers.TryControllers()
+	models.TryModels()
 }
---
+++ pkg/models/book-management-models.go
@@ -0,0 +1,9 @@
+package models
+
+import (
+	"fmt"
+)
+
+func TryModels() {
+	fmt.Println("from pkg/models/book-management-models.go")
+}
