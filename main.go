219d11eb75555aaa0e3814b58bc6f9be165c0684
DattaAnupam <aanupamdatta.1995@gmail.com>
2022-10-14 08:07:42 +0530

    register routes into main

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 	// declare new router
 	r := mux.NewRouter()
 	// Handle routing
-	// todo
+	routes.RegisterBookManagementRoutes(r)
 
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", r))
