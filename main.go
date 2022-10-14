cab6ff9a28421406b3a120a02289b46a3d63111e
DattaAnupam <aanupamdatta.1995@gmail.com>
2022-10-14 07:54:31 +0530

    declare handler functions

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"main/pkg/config"
-	"main/pkg/controllers"
 	"main/pkg/models"
 	"main/pkg/routes"
 	"main/pkg/utils"
@@ -16,12 +15,15 @@ import (
 func main() {
 	fmt.Println("basic structure")
 	routes.TryFunc()
-	controllers.TryControllers()
 	models.TryModels()
 	config.TryConfig()
 	utils.TryUtils()
 
+	// declare new router
 	r := mux.NewRouter()
+	// Handle routing
+	// todo
+
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
--- pkg/controllers/book-management-controllers.go
+++ pkg/controllers/book-management-controllers.go
@@ -2,8 +2,36 @@ package controllers
 
 import (
 	"fmt"
+	"net/http"
 )
 
-func TryControllers() {
-	fmt.Println("from pkg/controllers/book-management-controllers.go")
+// POST
+// Create a book
+var CreateBookHandler = func(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("from CreateBookHandler")
+}
+
+// GET
+// Get all books
+var GetAllBooksHandler = func(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("from GetAllBooksHandler")
+
+}
+
+// GET
+// Get a book by its Id
+var GetBookByIdHandler = func(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("from GetBookByIdHandler")
+}
+
+// PUT
+// Update a book detail
+var UpdateBookHandler = func(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("from UpdateBookHandler")
+}
+
+// DELETE
+// Delete a book
+var DeleteBookHandler = func(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("from DeleteBookHandler")
 }
