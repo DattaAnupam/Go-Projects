e576d3005086bd06ee7f447be8cd148c684ea3a1
DattaAnupam <aanupamdatta.1995@gmail.com>
2022-10-21 18:41:00 +0530

    implement routes

---
+++ pkg/routes/routes.go
@@ -0,0 +1,20 @@
+package routes
+
+import (
+	"github.com/anupam/crm-with-go-fiber-mysql/pkg/controllers"
+	"github.com/gofiber/fiber/v2"
+)
+
+var SetupRoutes = func(app *fiber.App) {
+	// Get all leads
+	app.Get("/api/lead", controllers.GetAllLeads)
+
+	// Get leads by Id
+	app.Get("/api/lead/:id", controllers.GetLeadByID)
+
+	// Create new lead
+	app.Post("/api/lead", controllers.CreateLead)
+
+	// Delete lead by id
+	app.Delete("/api/lead/:id", controllers.DeleteLeadByID)
+}
