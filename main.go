6e2cfbfd690f9b8d48c09c385d1f9ce1db2d724b
DattaAnupam <aanupamdatta.1995@gmail.com>
2022-10-22 12:21:09 +0530

    add url for CRM with go fiber, orm and mysql branch

--- main.go
+++ main.go
@@ -2,20 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
-
-	"github.com/anupam/crm-with-go-fiber-mysql/pkg/routes"
-
-	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	fmt.Println("initial code")
-	app := fiber.New()
-
-	// routes
-	routes.SetupRoutes(app)
-
-	// Start Server
-	log.Fatal(app.Listen(":8080"))
 }
