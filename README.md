8547662acdd2ae3d20d4a429231d83ebff35ac07
DattaAnupam <aanupamdatta.1995@gmail.com>
2022-11-07 08:38:19 +0530

    add race condition scenario

---
+++ README.md
@@ -0,0 +1,5 @@
+To check for race condition type - 
+
+```
+    go run -race .
+```
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 var msg string
 
-func updateMessage(s string) {
+func updateMessage(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	msg = s
 }
 
@@ -13,8 +17,12 @@ func printSomething() {
 }
 
 func main() {
+	var wg sync.WaitGroup
 	msg = "Hello, world!"
 
-	updateMessage("Hello, Universe!")
+	wg.Add(2)
+	go updateMessage("Hello, Universe!", &wg)
+	go updateMessage("Hello, Universe!", &wg)
+	wg.Wait()
 	printSomething()
 }
