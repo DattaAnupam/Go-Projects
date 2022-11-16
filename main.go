8da7f810e63eb123ffecdcd47443eaa0d9078832
DattaAnupam <aanupamdatta.1995@gmail.com>
2022-11-16 12:07:05 +0530

    init proj-structure

--- main.go
+++ main.go
@@ -2,16 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/anupam/armstrong/pkg/utility"
 )
 
 func main() {
-	// Take user input
-	var num int
-	fmt.Print("Enter a number : ")
-	fmt.Scanln(&num)
-
-	// Find Armstrong or not
-	utility.ArmstrongOrNot_1(num)
+	fmt.Println("Initial Code Structure")
 }
